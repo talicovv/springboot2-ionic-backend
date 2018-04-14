0448f36c7688867b3e87fbb0e94525722efaf03e
tales.viegas <soparacadastro80@gmail.com>
2018-04-14 15:27:53 -0300

    Pedido, EstadoPagamento e Pagamento

--- src/main/java/com/nelioalves/cursomc/repositories/PagamentoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/PagamentoRepository.java
@@ -1,18 +1,12 @@
 package com.nelioalves.cursomc.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.nelioalves.cursomc.domain.Endereco;
 
-
+import com.nelioalves.cursomc.domain.Pagamento;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
-
-	
+public interface PagamentoRepository extends JpaRepository<Pagamento, Integer> {
 	
 
 }
-
-
--- src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
+++ src/main/java/com/nelioalves/cursomc/repositories/PedidoRepository.java
@@ -1,18 +1,12 @@
 package com.nelioalves.cursomc.repositories;
 
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.nelioalves.cursomc.domain.Endereco;
 
-
+import com.nelioalves.cursomc.domain.Pedido;
 
 @Repository
-public interface EnderecoRepository extends JpaRepository<Endereco, Integer> {
-
-	
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
 	
 
 }
-
-
