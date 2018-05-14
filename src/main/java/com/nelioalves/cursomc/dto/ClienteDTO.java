c2f5fe5b2e8a5c1e5189143058179cfbef10d10d
talicovv <soparacastro80@gmail.com>
2018-05-14 20:33:53 -0300

    Put, delete, e get para cliente

---
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteDTO.java
@@ -0,0 +1,58 @@
+package com.nelioalves.cursomc.dto;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.nelioalves.cursomc.domain.Cliente;
+
+public class ClienteDTO implements Serializable {
+
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Integer id;
+	
+	@NotEmpty(message="You can't insert or update with empty!")
+	@Length(min=5, max=80, message="the length should be between 5 and 80 characters")
+	private String nome;	
+	
+	@NotEmpty(message="You can't insert or update with empty!")
+	@Email(message="It isn't e-mail valid!")
+	private String email;	
+	
+	
+	public ClienteDTO() {
+		
+	}
+	public ClienteDTO(Cliente obj) {
+		id = obj.getId();
+		nome = obj.getNome();
+		email = obj.getEmail();
+	}
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	
+
+}
