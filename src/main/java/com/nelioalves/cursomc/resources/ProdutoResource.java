e7426d0b3d65fbb2497b45d414387e6a627099bc
talicovv <soparacastro80@gmail.com>
2018-06-09 10:27:30 -0300

    cliente recupera somente seus pedidos

--- src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
+++ src/main/java/com/nelioalves/cursomc/resources/ProdutoResource.java
@@ -11,9 +11,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.nelioalves.cursomc.domain.Categoria;
 import com.nelioalves.cursomc.domain.Produto;
-import com.nelioalves.cursomc.dto.CategoriaDTO;
 import com.nelioalves.cursomc.dto.ProdutoDTO;
 import com.nelioalves.cursomc.resources.utils.URL;
 import com.nelioalves.cursomc.services.ProdutoService;
